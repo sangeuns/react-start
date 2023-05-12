67c18731f1508b144b9144d3ba8843d509333b07
sangeuns <sangeun.front@gmail.com>
2023-05-12 18:06:34 +0900

    usestate의 기본 변수 num과 setNum 중 num에 대하여 - num은 초기값 > ex let num = 0; 과 같은 개념으로 usestate의 기본값을 나타낸다

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import React, { useState } from "react";
 
 function App() {
   // onBtnIncreaseClick 함수를 만든다
-  const onBtnIncreaseClick=()=>{
-    console.log("버튼 클릭됨")
-  }
+  const onBtnIncreaseClick = () => {
+    console.log("버튼 클릭됨");
+  };
+  // usestate의 기본 변수 2가지 : [ num, setnum ]
+  // num은 usestate(num);의 초기값 -> 현재 num값은 '0'이다, setNum은 어떻게 증가 시킬것 인지를 선언하는 변수
+  const [num, setNum] = useState(0);
 
   // return 값으로 html 태그를 활용해 버튼 클릭시 onBtnIncreaseClick 함수가 실행되게 한다
   // {} : return 값 안에서 js코드, 주석 입력
   // <>, </> : html 태그를 사용하려면 해당 컨테인을 만들고 그 안에 코드 입력
-  
+
   return (
     <>
-    num:{0}
-    {/* {버튼을 누르면 onBtnIncreaseClick 함수가 실행한다} */}
-    <button onClick={onBtnIncreaseClick}> 증가 </button>
+      num:{num}
+      {/* {버튼을 누르면 onBtnIncreaseClick 함수가 실행한다} */}
+      <button onClick={onBtnIncreaseClick}> 증가 </button>
     </>
   );
 }
