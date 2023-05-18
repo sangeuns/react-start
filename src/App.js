fb4076bc485c77844edf4c79cc18d951fbf88557
sangeuns <sangeun.front@gmail.com>
2023-05-18 11:00:24 +0900

    입력상자 제어 - input을 활용해 제어하기

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React, { useState } from "react";
 
 function App() {
-  const [num, setNum] = useState(0);
-
-  const onBtnIncreaseClick = () => {
-    setNum(num + 1);
-  };
-  const onBtnDecreaseClick = () => {
-    setNum(num - 1);
-  };
-
+  // 실행할 함수를 적는다
+  let AppCallCount = 0;
+  AppCallCount++;
+  console.log(`app이 ${AppCallCount}번 실행됨`);
+  // return 값 안에 jsx 형식으로 제어요소를 입력한다
   return (
     <>
-      num:{num}
-      <br />
-      <button onClick={onBtnIncreaseClick}> 증가 </button>
-      <br />
-      <button onClick={onBtnDecreaseClick}> 감소 </button>
+      {/* {제어 요소 입력방식 : <input + 제어요소 + 제어요소... />} 
+    - type : number, email, date.. 등 입력 유형을 설정
+    - placeholder : 임시 힌트 텍스트
+    - defaultvalue : 입력 필드의 초기값*/}
+      <input type="text" placeholder="이름을 입력해주세요" defaultvalue={0} />
     </>
   );
 }
