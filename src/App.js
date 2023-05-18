a5a951020f51551b1ddacea9e6e9a6c042e3fb67
sangeuns <sangeun.front@gmail.com>
2023-05-18 12:13:52 +0900

    usestate를 이용해 제어 변경 허용하기 - 입력방식 : const [name, setName] = useState("park") - 해당 값의 형식에 알맞는 type 수정 - value는 변수 name으로 수정 - onchange 안에 setName(e.target.value);으로 변경 세팅하기

--- src/App.js
+++ src/App.js
@@ -5,22 +5,53 @@ function App() {
   let AppCallCount = 0;
   AppCallCount++;
   console.log(`app이 ${AppCallCount}번 실행됨`);
-  const name = "park";
+
+  //useState 형식으로 바꾸고 다른 변수 추가하기
+  // const [변수, 변수세팅] = useState(변수의 값);
+  const [name, setName] = useState("park");
+  const [age, setAge] = useState(30);
+  const [adress, setAdress] = useState("충남 공주시");
 
   // return 값 안에 jsx 형식으로 제어요소를 입력한다
   return (
     <>
-      {/* e란? 
-      - 발생하는 모든 이벤트, 사건을 말한다 -> 어디에 클릭했는지, 어떤 문자를 입력했는지 등등...
-      - e.target.value : 'e'를 받은 '당사자'(target -> input)의 일어났던 모든 값(value)을 뜻한다*/}
+      {/* 위에 추가한 useState의 변수와 변수 세팅을 input에 적용
+      - type, value 값 수정
+      - &nbsp; : 띄어쓰기
+      - <hr />로 구분선 추가 */}
       <input
         type="text"
         placeholder="이름을 입력해주세요"
         value={name}
         onChange={(e) => {
-          console.log(e.target.value);
+          console.log(`변경된 이름 log : ${e.target.value}`);
+          setName(e.target.value);
         }}
       />
+      &nbsp; / &nbsp; 이름 : {name}
+      <hr />
+      <input
+        type="number"
+        placeholder="나이를 입력해주세요"
+        value={age}
+        onChange={(e) => {
+          console.log(`변경된 나이 log : ${e.target.value}`);
+          setAge(e.target.value);
+        }}
+      />
+      &nbsp; / &nbsp; 나이 : {age}
+      <hr />
+      <input
+        type="text"
+        placeholder="주소를 입력해주세요"
+        value={adress}
+        onChange={(e) => {
+          console.log(`변경된 주소 log" : ${e.target.value}`);
+          setAdress(e.target.value);
+        }}
+      />
+      &nbsp; / &nbsp; 주소 : {adress}
+      <hr />
     </>
   );
 }
