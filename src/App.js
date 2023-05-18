38651034cc34024c6bbab0c412570ebac62d5344
sangeuns <sangeun.front@gmail.com>
2023-05-18 19:51:16 +0900

    usestate 컴포넌트 분리 - Control.js 새 파일 생성 후 코드 복제 - 기존 App.js에서 Control.js를 불러오는 코드로 수정

--- src/App.js
+++ src/App.js
@@ -1,49 +1,9 @@
-import React, { useState } from "react";
+import Control from "./Control";
 
 function App() {
-  let AppCallCount = 0;
-  AppCallCount++;
-  console.log(`app이 ${AppCallCount}번 실행됨`);
-
-  const [name, setName] = useState("park");
-  const [age, setAge] = useState(30);
-  const [adress, setAdress] = useState("충남 공주시");
-
   return (
     <>
-      <input
-        type="text"
-        placeholder="이름을 입력해주세요"
-        value={name}
-        onChange={(e) => {
-          console.log(`변경된 이름 log : ${e.target.value}`);
-          setName(e.target.value);
-        }}
-      />
-      &nbsp; / &nbsp; 이름 : {name}
-      <hr />
-      <input
-        type="number"
-        placeholder="나이를 입력해주세요"
-        value={age}
-        onChange={(e) => {
-          console.log(`변경된 나이 log : ${e.target.value}`);
-          setAge(e.target.value);
-        }}
-      />
-      &nbsp; / &nbsp; 나이 : {age}
-      <hr />
-      <input
-        type="text"
-        placeholder="주소를 입력해주세요"
-        value={adress}
-        onChange={(e) => {
-          console.log(`변경된 주소 log" : ${e.target.value}`);
-          setAdress(e.target.value);
-        }}
-      />
-      &nbsp; / &nbsp; 주소 : {adress}
-      <hr />
+      <Control />
     </>
   );
 }
